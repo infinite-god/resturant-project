0b442c1715b2b7561d8ad0d70aead254d96be5ef
David <dg7875506@gmail.com>
2025-10-12 22:27:37 +0100

    first commit

---
+++ asset/js/index.js
@@ -0,0 +1,10 @@
+document.addEventListener("DOMContentLoaded", function(){
+    document.getElementById("btnLogin").addEventListener("click", function(){
+        window.location.href = "login.html";
+    });
+});
+document.addEventListener("DOMContentLoaded", function(){
+    document.getElementById("btnSignup").addEventListener("click", function(){
+        window.location.href = "signup.html";
+    });
+});
---
+++ asset/js/signup.js
@@ -0,0 +1,10 @@
+document.addEventListener("DOMContentLoaded", function(){
+    document.getElementById("signupBtn").addEventListener("click", function(){
+        window.location.href = "index.html";
+    });
+});
+// document.addEventListener("DOMContentLoaded", function(){
+//     document.getElementById("loginBtn").addEventListener("click", function(){
+//         window.location.href = "index.html";
+//     });
+// });
