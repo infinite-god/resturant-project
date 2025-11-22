68ae79a6e37aaeb2eee6f2a5f353076ddb3123a8
David <dg7875506@gmail.com>
2025-11-22 16:37:48 +0100

    added the add to cart function

--- asset/js/food.js
+++ asset/js/food.js
@@ -19,5 +19,63 @@ document.addEventListener('click', (e) => {
     asideDashboard.classList.add('-translate-x-full');
   }
 });
-const searchBtn = document.getElementById('searchBtn')
-const searchInput = document.getElementById('searchInput')
+const searchBtn = document.getElementById('searchBtn');
+const searchInput = document.getElementById('searchInput');
+
+function setupCounter(decreaseId, increaseId, displayId){
+  let count = 0;
+  document.getElementById(decreaseId).onclick = function(){
+    if (count > 0){
+      count--
+    }
+    document.getElementById(displayId).value = count
+  };
+  document.getElementById(increaseId).onclick = function(){
+    count++
+    document.getElementById(displayId).value = count
+  }
+}
+// card 1
+setupCounter("decreaseBtn", "increaseBtn", "display");
+
+// card 2
+setupCounter("decreaseBtn2", "increaseBtn2", "display2");
+// card 3
+setupCounter("decreaseBtn3", "increaseBtn3", "display3");
+
+
+
+
+
+
+
+
+
+
+
+// let count = 0;
+// document.getElementById("decreaseBtn").onclick = function(){
+//   if (count > 0) {
+//      count-=1;
+//   }
+ 
+//   document.getElementById("display").value = count;
+// }
+// document.getElementById("increaseBtn").onclick = function(){
+//   count+=1;
+//   document.getElementById("display").value = count;
+// };
+// let counts = 0;
+// document.getElementById("decreaseBtn2").onclick = function(){
+//   if (counts > 0) {
+//      counts-=1;
+//   }
+ 
+//   document.getElementById("display2").value = count;
+// }
+// document.getElementById("increaseBtn2").onclick = function(){
+//   counts+=1;
+//   document.getElementById("display2").value = counts;
+// }
+
+
